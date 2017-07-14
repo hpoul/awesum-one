3753535a48e0d99c283bafcd94005b2c2dfe4e75
Herbert Poul <herbert@poul.at>
2017-07-14 23:18:41 +0200

    write generated yaml file to disk.

--- awesum-generator/src/main.ts
+++ awesum-generator/src/main.ts
@@ -1,13 +1,18 @@
 import {AwesomeLoader} from "./AwesomeLoader";
 import {GithubRepository} from "./GithubData";
-import winston = require('winston');
 import {logger} from "./logging";
 import * as jsyaml from "js-yaml";
+import winston = require('winston');
+import fsExtra = require('fs-extra');
+import * as path from "path";
 
 const awesomeAwesome = GithubRepository.fromPath('sindresorhus/awesome');
 const awesomeAndroid = GithubRepository.fromUrl('https://github.com/JStumpp/awesome-android');
 
+const outputDirectory = path.join(__dirname, '../out/');
+
 winston.cli();
+
 
 function main() {
     const loader = new AwesomeLoader();
@@ -16,7 +21,11 @@ function main() {
             logger.debug('Starting loading.');
             const list = await loader.loadAwesomeList(awesomeAndroid);
             logger.info('Got list: ', list);
-            logger.debug('as yaml: ' + jsyaml.safeDump(list));
+
+            await fsExtra.mkdirs(outputDirectory);
+            await fsExtra.writeFile(path.join(outputDirectory, 'awesum.yaml'), jsyaml.safeDump(list));
+
+            logger.info(`written into ${outputDirectory}`);
         } catch (error) {
             logger.error("Error while loading awesome list.", error);
         }
